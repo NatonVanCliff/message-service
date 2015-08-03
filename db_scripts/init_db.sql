63b30ba08e29e8a0e5e52993426cd5ad7c198605
NatonVanCliff <naton_van_cliff@mail.ru>
2015-08-03 15:28:15 +0300

    Misprint

--- db_scripts/init_db.sql
+++ db_scripts/init_db.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE msg_service;
 CREATE USER ms_user WITH PASSWORD 'ms_user';
-GRAND ALL PRIVILEGES ON DATABASE msg_service TO ms_user;
+GRANT ALL PRIVILEGES ON DATABASE msg_service TO ms_user;
